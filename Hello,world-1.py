ec8d31e09eaa001ba5496c6a34a6fc6a890d460d
mhamzaa <mhamzaa019@gmail.com>
2023-07-22 10:35:58 +0100

    commiting python changes in course work

--- Hello,world-1.py
+++ Hello,world-1.py
@@ -1,3 +1,16 @@
+'''
+#variables and formatting strings
+
 facts = input("name: ")
 
 print (f"hello, {facts}")
+'''
+#conditions
+n = int(input("number: "))
+if n > 0:
+    print("n is positive.")
+
+elif n < 0:
+    print("n is negative.")
+else:
+    print("n is not positive.")

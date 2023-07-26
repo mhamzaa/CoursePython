72287b4dd6410a23d077123583ba3f45e8a3d7b9
mhamzaa <mhamzaa019@gmail.com>
2023-07-26 15:34:01 +0100

    fourth pycommit

--- Hello,world-1.py
+++ Hello,world-1.py
@@ -1,10 +1,10 @@
-'''
+"""
 #variables and formatting strings
 
 facts = input("name: ")
 
 print (f"hello, {facts}")
-'''
+
 #conditions
 n = int(input("number: "))
 if n > 0:
@@ -14,3 +14,9 @@ elif n < 0:
     print("n is negative.")
 else:
     print("n is not positive.")
+"""
+
+#sequences
+name = "Hamza"
+
+print(name[0])
